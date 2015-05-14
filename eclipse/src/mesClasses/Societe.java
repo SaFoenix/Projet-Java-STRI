f64a4c07c5d79258f7aa3a338d6085d81e7d5c5a
guigui33 <guillaumecantal@gmail.com>
2015-05-14 22:13:51 +0200

    ajout methode

--- eclipse/src/mesClasses/Societe.java
+++ eclipse/src/mesClasses/Societe.java
@@ -30,14 +30,59 @@ public class Societe {
 		this.nom = nom;
 		locaux=new ArrayList<Local>();
 	}	
-	
-	public void ajouterLocal(String nomL){
-		Local nLocal=new Local(nomL);
-		locaux.add(nLocal);
+		
+	public void ajouterSalle(String nomL, Integer numero,Integer etage,Integer nombreOrdi){
+		Local locRech=rechercherLocal(nomL);
+		if(locRech==null){
+			System.out.println("Le local "+nomL+" n'existe pas. La salle, numero "+numero+", etage "+etage+" n'a pas été ajoutée.");
+			return;
+		}
+		locRech.ajouterSalle(numero, nombreOrdi, etage);
 	}
 	
-	public void ajouterSalle(String nomL, String nomS){
+	public Local rechercherLocal(String nomL){
 		
+		for(Local locRech : locaux){
+			if(nomL==locRech.getNom()){
+				return locRech;
+			}
+		}
+		return null;
+	}
+	public void ajouterLocal(String nomL){
+		Local nouvLocal=new Local(nomL);
+		for(Local loc : locaux){
+			if(loc.getNom()==nomL){
+				System.out.println("Le local "+nomL+" existe déjà.");
+				return;
+			}
+		}
+		locaux.add(nouvLocal);
+		System.out.println("Le local "+nomL+" a été ajouté.");
+		return;
+	}
+	
+	public void supprimerLocal(String nomL){
+		Local loc=rechercherLocal(nomL);		
+		if(loc!=null)
+			locaux.remove(loc);
+		else 
+			System.out.println("Le local "+nomL+" n'existe pas.");
+		return;		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String str;
+		str="Société: "+nom+"\n";
+		for(Local loc:locaux){
+			str+=loc.toString();
+			str+="\n\n";
+		}
+		return str;
 	}
 	
 }
