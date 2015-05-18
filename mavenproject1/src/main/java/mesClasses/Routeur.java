6efc80b41e9544f76d10749b42b9b82bd5499ac0
guigui33 <guillaumecantal@gmail.com>
2015-05-18 14:06:59 +0200

    ajout class dans netbeans

---
+++ mavenproject1/src/main/java/mesClasses/Routeur.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package mesClasses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author guigui
+ *
+ */
+public class Routeur extends Equipement {
+	private Ordinateur [] ordinateurs; 	
+	
+	/**
+	 * @param mac
+	 * @param nom
+	 * @param marque
+	 * @param power
+	 * @param os
+	 * @param nombrePorts
+	 * @param ordinateurs
+	 */
+	public Routeur(String mac, String nom, String marque,
+			boolean power, Os os, Integer nombrePorts) {
+		super(mac, nom, marque, power, os);
+		ordinateurs=new Ordinateur [nombrePorts];
+	}
+
+	/**
+	 * @return the nombrePorts
+	 */
+	public Integer getNombrePorts() {
+		return ordinateurs.length;
+	}
+
+	/**
+	 * 
+	 * @return le numero de port libre du routeur, -1 si aucun port 
+	 */
+	public Integer obtenirPortLibre(){
+		int i=0;
+		while(i<ordinateurs.length){
+			if(ordinateurs[i]==null){
+				return i;
+			}
+			i++;
+		}		
+		return -1;
+	}
+	
+	public void connecterOrdinateur(Ordinateur ordi){
+		Ordinateur ordiRech=rechercherOrdinateur(ordi.getMac());
+		if(ordiRech!=null){
+			System.out.println("L'ordinateur est déjà dans la salle.");
+			return;
+		}		
+		int portLibre=obtenirPortLibre();
+		if(portLibre!=-1){
+			System.out.println("ajout de l'ordinateur "+ordi.getNom() +" sur le routeur "+ this.getNom()+" sur le port "+ portLibre+".");
+			ordinateurs[portLibre]=ordi;
+		}
+		else System.out.println("Le routeur n'a pas de port de libre.");
+	}	
+	
+	public String toString() {
+		String str=super.toString();
+		str+="nombre de ports: "+getNombrePorts()+"\n";
+		for(int i=0;i<ordinateurs.length;i++){
+			str+="[Port "+i+"]\n";
+			if(ordinateurs[i]!=null)
+				str+=ordinateurs[i].toString()+"\n";
+			else str+="Vide\n";
+		}
+		return str;
+	}
+
+	public void activerDesactiverOrdinateur(boolean power) {
+				for(int i=0;i<ordinateurs.length;i++){
+					if(ordinateurs[i]!=null){
+						ordinateurs[i].activerDesactiverAppareil(power);
+					}
+				}
+	}	
+	
+	public Ordinateur rechercherOrdinateur(String mac){
+		for(int i=0;i<ordinateurs.length;i++){
+			if(ordinateurs[i]!=null && ordinateurs[i].getMac().equalsIgnoreCase(mac)){
+				return ordinateurs[i];
+			}
+		}
+		return null;
+	}
+	
+	public boolean activerDesactiverAppareil(String mac,boolean power){
+		Ordinateur ordiRech=rechercherOrdinateur(mac);
+		if(ordiRech!=null){
+			ordiRech.activerDesactiverAppareil(power);
+			return true;
+		}
+		return false;
+	}
+}
---
+++ mavenproject1/src/main/java/mesClasses/Salle.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package mesClasses;
+
+import java.util.ArrayList;
+
+/**
+ * @author STRI_JAVA
+ *
+ */
+public class Salle {
+	private Integer idSalle;
+	private Integer numero;
+	private Integer etage;
+	private Integer nombreOrdinateur;
+	private ArrayList<Routeur> routeurs;
+	private ArrayList<BorneSansFil> bornes;
+	
+	/**
+	 * @param idSalle
+	 * @param numero
+	 * @param etage
+	 * @param nombreOdinateur
+	 */
+	public Salle(Integer numero, Integer etage,Integer nombreOrdinateur) {
+		this.numero = numero;
+		this.etage = etage;
+		this.nombreOrdinateur = nombreOrdinateur;
+		routeurs=new ArrayList<Routeur>();
+		bornes=new ArrayList<BorneSansFil>();
+	}
+	
+	/**
+	 * @return the numero
+	 */
+	public Integer getNumero() {
+		return numero;
+	}
+
+	/**
+	 * @return the nombreOdinateur
+	 */
+	public Integer getNombreOrdinateur() {
+		return nombreOrdinateur;
+	}
+
+	/**
+	 * @param nombreOrdinateur the nombreOrdinateur to set
+	 */
+	public void setNombreOrdinateur(Integer nombreOrdinateur) {
+		this.nombreOrdinateur = nombreOrdinateur;
+	}
+
+	/**
+	 * @return the idSalle
+	 */
+	public Integer getIdSalle() {
+		return idSalle;
+	}
+
+	/**
+	 * @return the etage
+	 */
+	public Integer getEtage() {
+		return etage;
+	}
+
+	public String toString() {
+		String str="Salle ["+ etage + numero +"]\n";
+		str+="\tnombreOrdinateur: " + nombreOrdinateur+"\n";
+		str+="[Routeur]\n";
+		for(Routeur rout: routeurs){
+			str+=rout.toString()+"\n";
+		}
+		str+="[Bornes Wifi]\n";
+		for(BorneSansFil bor:bornes){
+			str+=bor.toString()+"\n";
+		}
+		return str;
+	}
+
+	public void ajouterRouteur(Routeur rout) {
+		for(Routeur obj : routeurs){
+			if(obj.getMac().equalsIgnoreCase(rout.getMac())){
+				System.out.println("Le retour "+rout.getMac()+" est déjà connecté dans la salle.");
+				return;
+			}				
+		}		
+		System.out.println("Le routeur "+rout.getMac()+" a été ajouté.");
+		routeurs.add(rout);		
+	}
+	
+	public Routeur rechercherRouteur(String mac){
+		for(Routeur rout : routeurs){
+			if(rout.getMac().equalsIgnoreCase(mac)){
+				return rout;
+			}
+		}	
+		return null;
+	}
+	
+	public BorneSansFil rechercherBorne(String mac){
+		for(BorneSansFil bo:bornes){
+			if(bo.getMac()==mac){
+				return bo;
+			}
+		}
+		return null;
+	}
+	public void connecterOrdinateur(Ordinateur ordi,String mac){
+		Routeur rout=rechercherRouteur(mac);
+		if(rout==null){
+			System.out.println("Le routeur n'existe pas.");
+		}
+		else{
+			rout.connecterOrdinateur(ordi);
+		}
+	}
+
+	public void ajouterBorneSansFil(BorneSansFil borne) {
+		for(BorneSansFil bo: bornes){
+			if(bo.getMac().equalsIgnoreCase(borne.getMac())){
+				System.out.println("La borne est déjà dans la salle.");
+				return;
+			}
+		}
+		System.out.println("La borne est ajoutée dans la salle.");
+		bornes.add(borne);
+	}
+
+	public void connecterTablette(Tablette tab, String macBorne) {
+		BorneSansFil boRech=rechercherBorne(macBorne);
+		if(boRech!=null){
+			boRech.connecterTablette(tab);
+		}
+		else System.out.println("Le borne n'existe pas.");
+	}
+
+	public void activerDesactiverAppareil(String mac,boolean power) {
+			Routeur rout=rechercherRouteur(mac);
+			if(rout !=null){
+				rout.activerDesactiverAppareil(power);
+				rout.activerDesactiverOrdinateur(power);
+				return;
+			}
+			else{
+				BorneSansFil bo=rechercherBorne(mac);
+				if(bo!=null){
+					bo.activerDesactiverAppareil(power);
+					bo.activerDesactiverTablette(power);
+					return;
+				}
+				else{
+					for(Routeur routIterateur :routeurs){
+						boolean trouve=routIterateur.activerDesactiverAppareil(mac,power);
+						if(trouve) return;
+					}
+					
+					for(BorneSansFil boIte :bornes){
+							boolean trouve=boIte.activerDesactiverAppareil(mac,power);
+							if(trouve) return;				
+					}	
+					System.out.println("L'équipement non trouvé.");
+				}
+			}
+	}
+}
