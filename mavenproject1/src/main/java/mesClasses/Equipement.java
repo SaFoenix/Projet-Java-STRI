fde7de75e5b9c939adfce672a8c4256785b2b194
guigui33 <guillaumecantal@gmail.com>
2015-05-20 13:59:09 +0200

    ajout doc

--- mavenproject1/src/main/java/mesClasses/Equipement.java
+++ mavenproject1/src/main/java/mesClasses/Equipement.java
@@ -1,130 +1,152 @@
 /**
- * 
+ *
  */
 package mesClasses;
 
 /**
- * La classe Equipement
- * 
+ * La classe Equipement qui represente la super classe equipement: La mac de
+ * l'equipement, non modifiable Le nom de l'equipement La marque de l'equipement
+ * L'etat de l'equipement (true=on;false=off) L'os de l'equipement
+ *
  * @author STRI_JAVA
  *
  */
 public class Equipement {
-	/**
-	 * L'adresse MAC de l'equipement, ne peut pas être modifée une fois l'equipement créé, est unique
-	 */
-	private String mac;
-	/**
-	 * Le nom de l'equipement, il peut être modifié
-	 */
-	private String nom;
 
-	/**
-	 * la marque de l'appareil, ne peut pas être modifiée apres la creation
-	 */
-	private String marque;
-	/**
-	 * definit si l'equipement est allumé ou eteint
-	 */
-	private boolean power;
-	private Os os;
+    /**
+     * L'adresse MAC de l'equipement, ne peut pas être modifée une fois
+     * l'equipement créé, est unique
+     */
+    private String mac;
+    /**
+     * Le nom de l'equipement, il peut être modifié
+     */
+    private String nom;
 
-	/**
-	 * @param mac
-	 * 				l'adresse MAC est unique
-	 * @param nom
-	 * 				Le nom de l'equipement
-	 * @param marque
-	 * @param power
-	 * @param os
-	 */
-	public Equipement(String mac, String nom,String marque, boolean power,Os os) {
-		this.mac = mac;
-		this.nom = nom;
-		this.marque = marque;
-		this.power = power;
-		this.os=os;
-	}
-	
-	public String getMac() {
-		return mac;
-	}
-	public void setMac(String mac) {
-		this.mac = mac;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    /**
+     * la marque de l'appareil, ne peut pas être modifiée apres la creation
+     */
+    private String marque;
+    /**
+     * definit si l'equipement est allumé ou eteint
+     */
+    private boolean power;
+    /**
+     * L'os de l'equipement
+     *
+     * @see Os
+     */
+    private Os os;
 
-	public String getMarque() {
-		return marque;
-	}
-	public void setMarque(String marque) {
-		this.marque = marque;
-	}
-	public boolean isPower() {
-		return power;
-	}
-	public void setPower(boolean power) {
-		this.power = power;
-	}
+    /**
+     * Le constructeur Equipement
+     *
+     * @param mac l'adresse MAC est unique
+     * @param nom Le nom de l'equipement
+     * @param marque La marque de l'equipement
+     * @param power état de l'equipement, on ou off
+     * @param os L'os de l'equipement
+     */
+    public Equipement(String mac, String nom, String marque, boolean power, Os os) {
+        this.mac = mac;
+        this.nom = nom;
+        this.marque = marque;
+        this.power = power;
+        this.os = os;
+    }
+/**
+ * 
+ * @return  la mac de l'equipement
+ */
+    public String getMac() {
+        return mac;
+    }
+/**
+ * retourne le nom de l'equipement
+ * @return Le nom de l'equipement
+ */
+    public String getNom() {
+        return nom;
+    }
+/**
+ * modifie le nom de l'equipement
+ * @param nom le nouveau nom de l'equipement
+ */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    /**
+     * 
+     * @return la marque de l'equipement
+     */
+    public String getMarque() {
+        return marque;
+    }
+/**
+ * retourne l'etat de l'equipement
+ * @return l'etat de l'equipement
+ */
+    public boolean isPower() {
+        return power;
+    }
+/**
+ * modifie l'etat de l'equipement
+ * @param power nouvel etat de l'equipement
+ */
+    public void setPower(boolean power) {
+        this.power = power;
+    }
 
-	/**
-	 * @return the os
-	 */
-	public Os getOs() {
-		return os;
-	}
+    /**
+     * @return the os
+     * @see Os
+     */
+    public Os getOs() {
+        return os;
+    }
 
-	/**
-	 * @param os the os to set
-	 */
-	public void setOs(Os os) {
-		this.os = os;
-	}
+    /**
+     * @param os the os to set
+     */
+    public void setOs(Os os) {
+        this.os = os;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		String str="Nom: "+getNom()+"\n";
-		str+="Mac: "+getMac()+"\tMarque: "+getMarque()+"\n";
-		str+="power: ";
-		str+=(isPower()?"on":"off");
-		str+="\n";
-		str+=os.toString()+"\n";
-		return str;
-	}
-	
-	public void activerDesactiverAppareil(boolean pow){
-		String str;
-		if(this instanceof Ordinateur){
-			str="L'ordinateur ";
-		}
-		else if(this instanceof Tablette){
-			str="La Tablette ";
-		}
-		else if(this instanceof Routeur){
-			str="Le routeur ";
-		}
-		else {
-			str="La borne Wifi ";
-		}
-		str+=(getNom()+" est "+(pow?"activé.":"désactivé."));		
-		System.out.println(str);
-	}
-        
-        public void miseAJour(Os newOs){
-            if(newOs.getNomOs().equalsIgnoreCase(os.getNomOs()) && newOs.getVersion().equalsIgnoreCase(os.getVersion())){
-                System.out.println("L'os est le même, mise à jour inutile.");
-            }
-            else {
-                System.out.println("Mise a jour de l'equipement.");
-                this.setOs(newOs);
-            }
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        String str = "Nom: " + getNom() + "\n";
+        str += "Mac: " + getMac() + "\tMarque: " + getMarque() + "\n";
+        str += "power: ";
+        str += (isPower() ? "on" : "off");
+        str += "\n";
+        str += os.toString() + "\n";
+        return str;
+    }
+
+    public void activerDesactiverAppareil(boolean pow) {
+        String str;
+        if (this instanceof Ordinateur) {
+            str = "L'ordinateur ";
+        } else if (this instanceof Tablette) {
+            str = "La Tablette ";
+        } else if (this instanceof Routeur) {
+            str = "Le routeur ";
+        } else {
+            str = "La borne Wifi ";
         }
+        str += (getNom() + " est " + (pow ? "activé." : "désactivé."));
+        System.out.println(str);
+    }
+
+    public void miseAJour(Os newOs) {
+        if (newOs.getNomOs().equalsIgnoreCase(os.getNomOs()) && newOs.getVersion().equalsIgnoreCase(os.getVersion())) {
+            System.out.println("L'os est le même, mise à jour inutile.");
+        } else {
+            System.out.println("Mise a jour de l'equipement.");
+            this.setOs(newOs);
+        }
+    }
 }
