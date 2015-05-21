a6db59eebaddc877c6194218427f734c186333ab
TomZzy <thomas.massip32@gmail.com>
2015-05-21 13:59:44 +0200

    Mysql

--- mavenproject1/src/main/java/mesClasses/MySql.java
+++ mavenproject1/src/main/java/mesClasses/MySql.java
@@ -15,7 +15,7 @@ public class MySql {
     private String host;
     private String pwd;
     private String url;
-    private Connection connection;
+    private Connection connexion;
             
     public MySql(){
         
@@ -24,23 +24,101 @@ public class MySql {
     this.url= "jdbc:mysql://localhost:3306/java?zeroDateTimeBehavior=convertToNull";
     }
     
-    public void Connection(){
-     
-/*      Class.forName("com.mysql.jdbc.Driver");
+    public void Connexion(){
+         try {
+      Class.forName("com.mysql.jdbc.Driver");
       System.out.println("Driver O.K.");
       
-      connection = DriverManager.getConnection(url, host, pwd);
+      connexion = DriverManager.getConnection(url, host, pwd);
       System.out.println("Connexion effective !");         
-      
-       */  
+      }
+      catch (Exception e) {
+      e.printStackTrace();
+    }     
+         
     }
     
-    public void AjoutSociete(String nom,String lieux) throws SQLException{
+    public void AjoutSociete(String nom,String lieux){
         
-        Statement st;
-        st=connection.createStatement();
-        String sql="INSERT INTO societe VALUES ('','"+nom+"','"+lieux+"')";
-        st.executeUpdate(sql);
+        Statement st = null;
+        int idMax=0;
+        try {
+            st=connexion.createStatement();
+        
+         ResultSet resultat2 = st.executeQuery( "SELECT  max(idSociete)  FROM societe;" );
+            while ( resultat2.next() ) {
+            idMax = resultat2.getInt( "max(idSociete)" );
+            idMax+=1;
+            }
+               
+        String sql="INSERT INTO societe VALUES ('"+idMax+"','"+nom+"','"+lieux+"')";
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
+    public void AjoutLocal(int numero , String lieux){
+        
+        Statement st = null;
+        int idMax=0;
+        try {
+            st=connexion.createStatement();
+        
+        ResultSet resultat2 = st.executeQuery( "SELECT  max(idLocal)  FROM Local;" );
+            while ( resultat2.next() ) {
+            idMax = resultat2.getInt("max(idLocal)");
+            idMax+=1;
+            }
+               
+        
+        String sql="INSERT INTO Local VALUES ('"+idMax+"','"+numero+"','"+lieux+"')";
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void AjoutSalle(int numero , int nbOrdi , int etage){
+        
+        Statement st = null;
+        int idMax=0;
+        try {
+            st=connexion.createStatement();
+        
+        ResultSet resultat2 = st.executeQuery( "SELECT  max(idSalle)  FROM Salle;" );
+            while ( resultat2.next() ) {
+            idMax = resultat2.getInt("max(idSalle)");
+            idMax+=1;
+            }
+               
+        
+        String sql="INSERT INTO Salle VALUES ('"+idMax+"','"+numero+"','"+nbOrdi+",'"+etage+"')";
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void AjoutOrdinateur(String nom , String mac , String marque, boolean power, String ram, String cpu , String gpu , String hdd){
+        
+        Statement st = null;
+        int idMax=0;
+        try {
+            st=connexion.createStatement();
+        
+        ResultSet resultat2 = st.executeQuery( "SELECT  max(idEquipement)  FROM Equipement;" );
+            while ( resultat2.next() ) {
+            idMax = resultat2.getInt("max(idEquipement)");
+            idMax+=1;
+            }
+        String sql="INSERT INTO equipement VALUES ('"+idMax+"','"+nom+"','"+mac+"','"+marque+"',"+power+");";
+            st.executeUpdate(sql);
+        String sqlOrdinateur="INSERT INTO ordinateur VALUES ('"+idMax+"','"+ram+"','"+cpu+"','"+gpu+"','"+hdd+"')";
+            st.executeUpdate(sqlOrdinateur);
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
