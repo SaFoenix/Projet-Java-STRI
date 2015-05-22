2737eaf01fee931ad46b611f6ffe03a94acc5d64
TomZzy <thomas.massip32@gmail.com>
2015-05-22 23:31:05 +0200

    Requete + Verification pour eviter les doublons

--- mavenproject1/src/main/java/mesTest/Test.java
+++ mavenproject1/src/main/java/mesTest/Test.java
@@ -16,15 +16,17 @@ public class Test {
 	 */
 	public static void main(String[] args){
                        Societe societe;
+                       boolean TestOrdinateur;
                        ArrayList<Local> locaux = new ArrayList<>();
                        ArrayList<Salle> salle = new ArrayList<>();
                        MySql bdd = new MySql();
                        bdd.Connexion();
                        //bdd.AjoutSociete("Stri","Toulouse");
-                       //bdd.AjoutLocal("STRI","Blabla","Toulouse");
-                       //bdd.AjoutSalle("Amphi", 1,20,1);
-                       //bdd.AjoutSalle("Lit", 1, 1, 1);
-                       //bdd.AjoutOrdinateur("TomZzy", "Asus", "AdresseMac",true, "500", "400", "300", "1TO");
+                       //bdd.AjoutLocal("STRI","Amphi","Toulouse");
+                       //bdd.AjoutSalle("Salle 8",4,20,1);
+                       bdd.AjoutOrdinateur(1,"GuiGui","2001::" ,"Asus" ,true, "500", "400", "300", "1TO");
+                       //TestOrdinateur= bdd.VerifierEquipement("FF:FF:FF:FF:F:FF");
+                       //System.out.println(TestOrdinateur);
                        //societe=bdd.RecupererSociete("STRI");
                        //System.out.println(societe);
                        //locaux=bdd.SocieteLocal("STRI");
@@ -32,7 +34,7 @@ public class Test {
                        //salle=bdd.LocalSalle("Amphi");
                        //System.out.println(salle);
                        //System.out.println(societe);
-                       bdd.AjoutOrdinateur(1,"TomZzy", "Asus", "AdresseMac",true, "500", "400", "300", "1TO");
+                       
 	}
 
 }
